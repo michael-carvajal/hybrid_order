e9f4a4c21e37c1b7fae85fbbd110e107c023db20
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-05-30 22:05:25 -0400

    able to choose store

--- src/tirehub/index.js
+++ src/tirehub/index.js
@@ -15,7 +15,7 @@ async function searchForItem(page, itemNumber, quantity) {
     "Search for Size and Brand, Model etc. or Item #"
   );
   await itemInput.waitFor({ state: "visible" });
-  itemInput.fill(itemNumber);
+  await itemInput.fill(itemNumber);
   await page.keyboard.press("Enter");
   //   await page.getByRole("textbox", { name: "Qty" }).fill(quantity);
   //   console.log("we found qty");
@@ -38,7 +38,8 @@ async function orderFromTirehub(
   await login(username, password, page);
   let storeId;
   try {
-    storeId = await getStoreId(storeNumber);
+    await searchForItem(page, itemNumber, quantity);
+    storeId = getStoreId(storeNumber);
     const changeStore = await page.getByRole("link", { name: "Change" });
     await changeStore.waitFor({ state: "visible" });
     await changeStore.click();
@@ -47,7 +48,6 @@ async function orderFromTirehub(
     await storeInput.fill(storeId);
     await page.locator(".store-item__action a").click();
 
-    await searchForItem(page, itemNumber, quantity);
     await page.fill("#customerPO", poNumber);
     // Wait for the confirmation page to load
     await page.waitForSelector(".order-confirmation-message strong"); // Replace with the actual selector for the confirmation number
