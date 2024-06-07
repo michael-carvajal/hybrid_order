52472123afaf669f1754d4efa4e002549a6b1770
Michael Carvajal <michaelcarvajal.work@gmail.com>
2024-06-07 17:51:22 -0400

    set up main file for k and  m

--- src/k&m/index.js
+++ src/k&m/index.js
@@ -1,13 +1,9 @@
-const getStoreId = require("./getStoreId")
+const getStoreId = require("./getStoreId");
 
 async function login(username, password, page) {
-  // Fill in the username
-  await page.fill("#usernameTextBox", username);
-
-  // Fill in the password
-  await page.fill("#passwordTextBox", password);
-  await page.getByRole("button").click();
-  await page.getByRole("textbox");
+  await page.getByLabel("Email address:", { exact: true }).fill(username);
+  await page.getByLabel("Password:").fill(password);
+  await page.getByRole("button", { name: "Sign in" }).click();
 }
 async function searchForItem(page, itemNumber, quantity) {
   await page.fill("#tirepartField", itemNumber);
@@ -20,7 +16,7 @@ async function searchForItem(page, itemNumber, quantity) {
   await page.keyboard.press("Enter");
 }
 
-async function orderFromUSA(
+async function orderFromKAndM(
   page,
   url,
   storeNumber,
@@ -32,16 +28,11 @@ async function orderFromUSA(
   pickup
 ) {
   await page.goto(url);
-  await page
-    .getByLabel("Email address:", { exact: true })
-    .fill(username);
-  await page.getByLabel("Password:").fill(password);
-  await page.getByRole("button", { name: "Sign in" }).click();
-  
-  await page.locator('#custSearch').fill(storeNumber + " PR")
+  await login(username, password, page);
+  await page.locator("#custSearch").fill(storeNumber + " PR");
   await page.getByLabel("Part Number").click();
   await page.getByLabel("Part Number").fill("212290");
   await page.getByLabel("Part Number").press("Enter");
 }
 
-module.exports = orderFromUSA;
+module.exports = orderFromKAndM;
