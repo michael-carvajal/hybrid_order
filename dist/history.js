5bd7c2b162d96e1e73b053e19420ba782923fec4
Michael Carvajal <michaelcarvajal.work@gmail.com>
2024-06-28 11:58:00 -0400

    removed dummy data

--- dist/history.js
+++ dist/history.js
@@ -1,50 +1,42 @@
 // history.js
 
 window.addEventListener("load", (event) => {
-  console.log("page is fully loaded");
-  const historyContainer = document.getElementById("history-container");
-
-  let historyState = localStorage.getItem("historyState");
-  if (!historyState) {
-    historyState = dummyData;
-  } else {
-    historyState = JSON.parse(historyState);
-  }
-  createHistoryDivs(historyState, historyContainer);
-
-  document
-    .getElementById("add-history-btn")
-    .addEventListener("click", addHistoryItem);
-});
-
-const createHistoryDivs = (historyState, historyContainer) => {
-  for (const [store, value] of Object.entries(historyState)) {
-    console.log(`${store}: ${value}`);
-    const historyDiv = document.createElement("div");
-    historyDiv.innerText = `${store} history div: ETA ${value.eta}, Confirm ${value.confrim}, Price ${value.price}`;
-    historyContainer.appendChild(historyDiv);
-  }
-};
-
-const dummyData = {
-  1: { eta: "1234", confrim: "yes", price: 1234 },
-  2: { eta: "1234", confrim: "yes", price: 1234 },
-  100: { eta: "1234", confrim: "yes", price: 1234 },
-};
-
-const addHistoryItem = () => {
-  let historyState = localStorage.getItem("historyState");
-  if (!historyState) {
-    historyState = dummyData;
-  } else {
-    historyState = JSON.parse(historyState);
-  }
-
-  const newKey = Object.keys(historyState).length + 1;
-  historyState[newKey] = { eta: "5678", confrim: "no", price: 5678 };
-
-  localStorage.setItem("historyState", JSON.stringify(historyState));
-  const historyContainer = document.getElementById("history-container");
-  historyContainer.innerHTML = "";
-  createHistoryDivs(historyState, historyContainer);
-};
+    console.log("page is fully loaded");
+    const historyContainer = document.getElementById("history-container");
+  
+    let historyState = localStorage.getItem("historyState");
+    if (!historyState) {
+      historyState = { history: [] };
+    } else {
+      historyState = JSON.parse(historyState);
+    }
+    createHistoryDivs(historyState.history, historyContainer);
+  
+    document.getElementById("add-history-btn").addEventListener("click", addHistoryItem);
+  });
+  
+  const createHistoryDivs = (historyArray, historyContainer) => {
+    historyContainer.innerHTML = '';
+    historyArray.forEach((historyItem, index) => {
+      const historyDiv = document.createElement("div");
+      historyDiv.innerText = `History ${index + 1}: ETA ${historyItem.eta}, Confirm ${historyItem.confirm}, Price ${historyItem.price}`;
+      historyContainer.appendChild(historyDiv);
+    });
+  };
+  
+  const addHistoryItem = () => {
+    let historyState = localStorage.getItem("historyState");
+    if (!historyState) {
+      historyState = { history: [] };
+    } else {
+      historyState = JSON.parse(historyState);
+    }
+  
+    const newHistoryItem = { eta: "5678", confirm: "no", price: 5678 };
+    historyState.history.push(newHistoryItem);
+  
+    localStorage.setItem("historyState", JSON.stringify(historyState));
+    const historyContainer = document.getElementById("history-container");
+    createHistoryDivs(historyState.history, historyContainer);
+  };
+  
